560585cbb9cfb59ef7010f54bab9a9e8db1f2ab8
bruhensant <bruhensant@gmail.com>
2021-03-09 18:57:14 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -21,15 +21,11 @@ class Pessoa():
         self.email = email
         self.matricula = matricula
 
-    # 1 get <|
-
     def obterNome(self):
         print(self.nome)
 
     def obterEmail(self):
         print(self.email)
-    
-    # 2 set <|
 
     def alterarTelefone(self, numero):
         self.telefone = numero
@@ -37,38 +33,8 @@ class Pessoa():
     def alterarMatricula(self, novaM):
         self.matricula = novaM
 
-    # 3 validar matricula
-
     def validarMatricula(self):
-        if len(str(self.matricula)) < 4:
-            print('Matrícula inválida')
+        if len(str(self.matricula)) <4:
+            print('Matrícula inválida ')
         else:
             print('Matrícula válida')
-
-
-#######################################################################
-#                          >=> EXECUÇÕES <=<                          #
-#######################################################################
-
-a1 = Pessoa('Furlano', 1111-1111, 'email1@email.com', 1111)
-a2 = Pessoa('Sicrano', 2222-2222, 'email2@email.com', 2222)
-a3 = Pessoa('Beltrano', 3333-3333, 'email3@email.com', 3333)
-
-contatos = Agenda()
-
-contatos.inserirPessoa(a1)
-contatos.inserirPessoa(a2)
-contatos.inserirPessoa(a3)
-
-contatos.imprimirAgenda()
-
-contatos.removerPessoa('Sicrano')
-
-print('==============================')
-
-contatos.imprimirAgenda()
-
-print('==============================')
-
-a1.obterNome()
-a3.obterEmail()
