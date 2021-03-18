d0da33e73c5a7d0670c808100e5c7cec1bb832f1
bruhensant <bruhensant@gmail.com>
2021-03-18 18:40:08 -0300

    Create 04.py

---
+++ Exercícios/04.py
@@ -0,0 +1,21 @@
+# A fim de representar empregados em uma firma, crie uma classe chamada
+# Empregado que herda as características de uma classe Pessoa, incluindo as
+# seguintes características:
+#    – Atributos
+#        o Salário mensal
+#        o Cargo
+#        o Endereço: o endereço deve ser um objeto da classe Endereço.
+#    – Construtores:
+#        > o Um constructor para inicializar os três atributos e os dados pessoais
+#        para classe Pessoa
+#        > o Um constructor para inicializar apenas o salário os dados pessoais
+#        para classe Pessoa
+#        > o Um constructor para inicializar salário e cargo os dados pessoais para
+#        classe Pessoa
+#    – Métodos
+#        o Obter valores dos atributos
+#        o Atualizar os valores dos atributos.
+#    - Restrições
+#        > o Um salário não pode ter seu valor negativo. Logo, ao atualizar salário,
+#        deve-se verificar sua consistência
+
